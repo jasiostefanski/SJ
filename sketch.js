b010d2c15cfc58a361015dc50a78abeee05c9fe7
pengb <pengb@vcu.edu>
2016-02-26 02:52:26 -0500

    fixed

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
-var y1 = 0;
-var x1 = 0;
+var y1 = [];
+var x1 = [];
 
-var y2 = 960;
-var x2 = 700;
-var xspeed = 18;
-var yspeed = 12;
+var y2 = [];
+var x2 = [];
+
+var x1speed = [];
+var y1speed = [];
+
+var x2speed = [];
+var y2speed = [];
+
 var i = 0;
 
 // The statements in the setup() function 
@@ -16,6 +21,22 @@ function setup() {
   frameRate(10);
   img = loadImage("assets/kruger.jpg");
   img2 = loadImage("assets/323.jpg");
+  
+  for(i=0;i<10;i++){
+    x1speed[i]=18;
+    y1speed[i]=12;
+    
+    x2speed[i]=10;
+    y2speed[i]=18;
+    
+    if(i==0){
+      x1[i]=0;y1[i]=0;
+      x2[i]=0;y2[i]=0;
+    } else {
+      x1[i]=0-i*x1speed[i]; y1[i]=0-i*y1speed[i];
+      x2[i]=0-i*x2speed[i]; y2[i]=0-i*y2speed[i];
+    }
+  }
 }
 // The statements in draw() are executed until the 
 // program is stopped. Each statement is executed in 
@@ -26,8 +47,8 @@ function draw() {
   move();
   for (i=0;i<10;i++){
     imageMode(CENTER);
-    image(img,x1-i*xspeed,y1-i*yspeed,300,450);
-    image(img2,x2-i*xspeed,y2-i*yspeed,305,300);
+    image(img,x1[i],y1[i],300,450);
+    image(img2,x2[i],y2[i],305,300);
     
   }
   // ellipse(x,y,40,40);
@@ -36,23 +57,39 @@ function draw() {
 } 
 
 function move() {
-  x1 = x1 + xspeed;
-  x2 = x2 - xspeed;
-  if (x1 > (width+300) || x1 < 0-300)  {
-    xspeed = -xspeed;
-  }
+  for(i=0;i<10;i++){
+  
+    x1[i] = x1[i] + x1speed[i];
+    if (x1[i] > (width) || x1[i] < 0)  {
+      x1speed[i] = -x1speed[i];
+      if ((x1[i]+x1speed[i])<0){
+        x1speed[i] = -x1speed[i];
+      }
+    }
 
-  y1 = y1 + yspeed;
-  y2 = y2 - yspeed;
-  if (y1 > (height+450) || y1 < 0-450)  {
-    yspeed = -yspeed;
+    y1[i] = y1[i] + y1speed[i];
+    if (y1[i] > (height) || y1[i] < 0)  {
+      y1speed[i] = -y1speed[i];
+      if ((y1[i]+y1speed[i])<0){
+        y1speed[i] = -y1speed[i];
+      }
+    }
+    
+    x2[i] = x2[i] + x2speed[i];
+    if (x2[i] > (width) || x2[i] < 0)  {
+      x2speed[i] = -x2speed[i];
+      if ((x2[i]+x2speed[i])<0){
+        x2speed[i] = -x2speed[i];
+      }
+    }
+
+    y2[i] = y2[i] + y2speed[i];
+    if (y2[i] > (height) || y2[i] < 0)  {
+      y2speed[i] = -y2speed[i];
+      if ((y2[i]+y2speed[i])<0){
+        y2speed[i] = -y2speed[i];
+      }
+    }
+    
   }
 }
-
-function windowResized() {
-  resizeCanvas(960, 700);
-}
-
-// function mousePressed() {
-// background(255,0,0);
-// }
